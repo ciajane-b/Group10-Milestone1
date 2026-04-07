a147f00dc02c819a18f1f7efe0535c812c3ae00d
santinoarevalo66-hash <santinoarevalo66@gmail.com>
2026-04-07 15:50:49 +0800

    Create UserSession.java

---
+++ UserSession.java
@@ -0,0 +1,6 @@
+public class UserSession {
+    private String username, rank;
+    public UserSession(String u, String r) { this.username = u; this.rank = r; }
+    public String getUsername() { return username; }
+    public String getRank() { return rank; }
+}
